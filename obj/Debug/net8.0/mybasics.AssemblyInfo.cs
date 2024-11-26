2d4da63687a03cb26ad29640a55178fb6de8fb06
Cahidenuryalcin <cnuryal@gmail.com>
2024-11-26 21:07:55 +0300

    b2

--- obj/Debug/net8.0/mybasics.AssemblyInfo.cs
+++ obj/Debug/net8.0/mybasics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mybasics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac1b079bddae7773005d984f5eebf26fcf695a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29bea0a0b582e9a5473a40a221f83e2872979de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("mybasics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mybasics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
